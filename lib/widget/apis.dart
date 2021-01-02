8e5a9bbfdf285662548e08aec1c77a55c63e5a09
sai-yamavaram <saikarthiksep01@gmail.com>
2021-01-02 13:25:23 +0530

    fixing buttons

--- lib/widget/apis.dart
+++ lib/widget/apis.dart
@@ -83,7 +83,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Update Data Example',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: const Color(0xFF757575),
       ),
       home: Scaffold(
         appBar: AppBar(
